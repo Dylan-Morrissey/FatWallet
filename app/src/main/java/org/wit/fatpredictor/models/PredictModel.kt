32dfad339710d0801f73606fc8113f91456dbe26
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2020-01-27 14:14:16 +0000

    Added a new database and DAO

--- app/src/main/java/org/wit/fatpredictor/models/PredictModel.kt
+++ app/src/main/java/org/wit/fatpredictor/models/PredictModel.kt
@@ -1,16 +1,19 @@
 package org.wit.fatpredictor.models
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class PredictModel(var id: Long = 0,
-                          var weight: String = "",
-                          var height: String = "",
-                          var image: String = "",
-                          var age : Int = 0,
-                          var gender: Char = 'M',
-                          var bodyfat: String = "0-0") : Parcelable
+@Entity
+data class PredictModel(@PrimaryKey(autoGenerate = true) var id: Long = 0,
+                        var weight: String = "",
+                        var height: String = "",
+                        var image: String = "",
+                        var age : Int = 0,
+                        var gender: Char = 'M',
+                        var bodyfat: String = "0-0") : Parcelable
 
 
 
