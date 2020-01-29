2b3689de939fa8d92b33c6b23acdeae1e77da7ec
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2020-01-29 20:44:01 +0000

    Added Firebase sign in

--- app/src/main/java/org/wit/fatpredictor/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/fatpredictor/activities/LoginActivity.kt
@@ -2,7 +2,10 @@ package org.wit.fatpredictor.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -12,9 +15,11 @@ import org.wit.fatpredictor.R
 class LoginActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var auth:FirebaseAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         info("Login Activity Started...")
+        progressBar.visibility = GONE
 
         /*
         btnForgotPassword.setOnClickListener() {
@@ -29,15 +34,26 @@ class LoginActivity : AppCompatActivity(), AnkoLogger {
         }
         */
         btnSignIn.setOnClickListener() {
+            showProgress()
             val email = loginEmail.text.toString()
             val password = loginPassword.text.toString()
-            if(email == "" || password == "") {
-                toast("Please enter email and password.")
-            } else{
-                val intent = Intent(baseContext, PredictionListActivity::class.java)
-                startActivity(intent)
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {task ->
+                if (task.isSuccessful){
+                    val intent = Intent(baseContext, PredictionListActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    toast("Sign In Failed")
+                }
             }
-
+            hideProgress()
         }
+    }
+
+    fun showProgress() {
+        progressBar.visibility = VISIBLE
+    }
+
+    fun hideProgress(){
+        progressBar.visibility = GONE
     }
 }
